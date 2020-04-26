7a7e996ae02af8f3b68cae6a41c5d88ece0e8cad
WatPro <31579668+WatPro@users.noreply.github.com>
2020-04-26 10:01:49 +0800

    Update L601_TransactSQL.sql

--- 202004_SQL/L601_TransactSQL.sql
+++ 202004_SQL/L601_TransactSQL.sql
@@ -1,5 +1,6 @@
 -- SELECT @@version; 
--- Microsoft SQL Server 2017 (RTM-CU19) (KB4535007) - 14.0.3281.6 (X64) \n\tJan 23 2020 21:00:04 
+-- Microsoft SQL Server 2017 (RTM-CU19) (KB4535007) - 14.0.3281.6 (X64) 
+--     Jan 23 2020 21:00:04 
 --     Copyright (C) 2017 Microsoft Corporation
 --     Express Edition (64-bit) on Linux (Ubuntu 16.04.6 LTS)
 
