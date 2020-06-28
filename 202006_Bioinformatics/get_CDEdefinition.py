f8be5790240db2dd4b7cd0eb0bf872f74435a4ac
WatPro <31579668+WatPro@users.noreply.github.com>
2020-06-28 17:21:15 +0800

    Create get_CDEdefinition.py

---
+++ 202006_Bioinformatics/get_CDEdefinition.py
@@ -0,0 +1,13 @@
+import json
+import glob
+
+if ( __name__ == '__main__' ):
+  for filepath in glob.glob('02raw_xml/deIdseq_*.json'):
+    with open(filepath) as f:
+      data = json.load(f)
+      publicId=data['dataElementConcept']['selectedDataElement']['publicId']
+      perferred=data['dataElementConcept']['selectedDataElement']['preferredQuestionText']
+      elementDefinition=data['dataElementConcept']['selectedDataElement']['definition']
+      conceptDefinition=data['dataElementConcept']['dataElementConceptDetails']['definition']
+      print(publicId,perferred,elementDefinition,conceptDefinition,sep='\t')
+
