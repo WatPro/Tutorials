1547ceadeba96fafef4624e1364d64ab2a6d9c11
Water Pro <@>
2020-07-01 17:12:58 +0000

    Updated

--- 202006_Bioinformatics/script_extract_table.py
+++ 202006_Bioinformatics/script_extract_table.py
@@ -1,29 +1,38 @@
+#/usr/bin/python3
+
 import xml.sax
 import re
 import glob
+
+filename = ''
 
 class ContentHandler( xml.sax.ContentHandler ): 
   def __init__(self): 
     self.isBody            = False
     self.currentTag        = ''
     self.CommonDataElement = ''
+    self.preferred         = ''
+    print('filename','tag','cde','value','preferred_name',sep='\t')
     
   def startElement(self, tag, attributes): 
     self.currentTag = tag
     if 'cde' in attributes: 
       self.CommonDataElement = attributes['cde']
+    if 'preferred_name' in attributes: 
+      self.preferred         = attributes['preferred_name']
     if tag == 'laml:patient':
       self.isBody = True 
   
   def characters(self, content):
     thisContent = content.strip() 
-    thisTag     = re.sub('^.+:','',self.currentTag)
+    thisTag     = self.currentTag
     if self.isBody and (thisTag != '') and not (thisContent == ''): 
-      print(filename,thisTag,self.CommonDataElement.strip(),sep='\t')
+      print(filename,thisTag,self.CommonDataElement,thisContent,self.preferred,sep='\t')
   
   def endElement(self, tag): 
     self.currentTag        = ''
     self.CommonDataElement = ''
+    self.preferred         = ''
     if tag == 'laml:patient':
       self.isBody = False
 
@@ -33,6 +42,8 @@ if ( __name__ == '__main__'):
   Handler = ContentHandler()
   parser.setContentHandler( Handler )
   
-  for filepath in glob.glob('02raw_xml/*.xml'): 
+  for filepath in glob.glob('02raw_xml/*/*.xml'): 
     filename = re.sub('^.*/','',filepath)
     parser.parse(filepath) 
+
+
