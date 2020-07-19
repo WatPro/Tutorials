1685ae937fd2caa6c32c5c2f1cc75abf28a6ff7a
WatPro <31579668+WatPro@users.noreply.github.com>
2020-07-20 01:03:44 +0800

    Update extract.ps1

--- 202007_VersionUnicodeCharacterDatabase/05test/extract.ps1
+++ 202007_VersionUnicodeCharacterDatabase/05test/extract.ps1
@@ -1,37 +1,34 @@
 
-[System.Text.UTF8Encoding]$UTF8       = [System.Text.Encoding]::UTF8 ;
-[System.Text.UnicodeEncoding]$Unicode = [System.Text.Encoding]::Unicode ;
+Import-Module -Name './module.psm1' -Function 'Get-UTF8Code';
+
 Get-ChildItem |
   Where-Object {
-    $_.Extension -eq '.sample'
+    $_.Extension -eq '.csv'
   } | 
   ForEach-Object -Process {
-    [System.String]$BaseName = $_.BaseName ; 
-    [System.String]$FullName = $_.FullName ; 
-    Get-Content -LiteralPath "${FullName}" -Encoding UTF8; 
-  } | 
-  ForEach-Object -Process {
-    [System.String]$OneLine = $_ ; 
-    [System.Int32]$Len      = $OneLine.Length; 
-    for ([System.Int32]$ii = 0; $ii -lt $Len; $ii+=1) {
-      [System.Char]$cc = [System.Char]$OneLine[$ii]; 
-      [System.Int64]$integer_uni  = (
-        $Unicode.GetBytes($cc) | 
-        ForEach-Object -Begin {
-          [System.Int64]$multi=1;[System.Int64]$code=0;
-        } -Process {
-          $code+=$_*$multi;$multi*=256
-        } -End{$code}
-      ); 
-      [System.String]$string_utf8 = (
-        ($UTF8.GetBytes($cc) | ForEach-Object -Process {$_.toString('X')}) -join ' '
-      ); 
+    [System.String]$BaseName  = $_.BaseName ; 
+    [System.String]$FullName  = $_.FullName ; 
+    [System.IO.StreamReader]$Reader = (
+      New-Object -TypeName System.IO.StreamReader -ArgumentList "${FullName}"
+    ); 
+    [System.IO.Stream]$Stream = $Reader.BaseStream; 
+    while($true) {
+      [System.Int32]$Code = (Get-UTF8Code -Stream $Stream); 
+      if( ${Code} -le 0 ) {
+        break;
+      }
       1 | 
-      Select-Object @{label='unicode';expression={$integer_uni}},
-                    @{label='utf8';expression={$string_utf8}}, 
-                    @{label='file';expression={$BaseName}}, 
-                    @{label='character';expression={$cc}};
+      Select-Object -Property @{label='unicode';expression={${Code}}},
+                              @{label='file';expression={$BaseName}};
     }
+    $Reader.Close();
   } | 
-  Where-Object { $_.unicode -ge 0x80 } | 
-  Export-Csv -LiteralPath './statistics.csv' -Encoding utf8; 
+  Where-Object {$_.unicode -ge 0x80} |
+  Export-Csv -LiteralPath './statistics.tsv' -Delimiter "`t";
+
+Import-Csv -Delimiter "`t" -LiteralPath './statistics.tsv' |
+  Select-Object -Property @{
+      label      = 'unicode';
+      expression = {[System.Convert]::ToInt32($_.unicode);}
+    } |
+  Sort-Object -Property unicode -Unique;
