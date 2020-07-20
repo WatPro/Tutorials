3a25ca01784e0f90c8661debbc34e16c6eef7029
WatPro <31579668+WatPro@users.noreply.github.com>
2020-07-21 01:36:12 +0800

    Update module.psm1

--- 202007_VersionUnicodeCharacterDatabase/05test/module.psm1
+++ 202007_VersionUnicodeCharacterDatabase/05test/module.psm1
@@ -3,15 +3,15 @@ function Get-UTF8ByteType {
     [System.Byte]$Byte
   );
   if(       ($Byte -band 0x80) -eq    0) {
-    [System.Int32]1;
+    [System.Int16]1;
   } elseif (($Byte -band 0xC0) -eq 0x80) { 
-    [System.Int32]0;
+    [System.Int16]0;
   } elseif (($Byte -band 0xE0) -eq 0xC0) { 
-    [System.Int32]2;
+    [System.Int16]2;
   } elseif (($Byte -band 0xF0) -eq 0xE0) { 
-    [System.Int32]3;
+    [System.Int16]3;
   } elseif (($Byte -band 0xF8) -eq 0xF0) { 
-    [System.Int32]4;
+    [System.Int16]4;
   }
 }
 function Get-UTF8ByteValue {
@@ -30,36 +30,50 @@ function Get-FileUTF8Bytes {
   Param ( 
     [System.IO.Stream]$Stream
   ); 
-  [System.Int16]$Byte1 = $Stream.ReadByte(); 
-  if( $Byte1 -eq -1 ) {return [System.Byte[]]@();}
-  [System.Int32]$type  = (Get-UTF8ByteType -Byte $Byte1);
+  [System.Byte[]]$Empty = @(); 
+  [System.Int16]$Byte   = $Stream.ReadByte(); 
+  if( $Byte -eq -1 ) {return $Empty;}
+  [System.Int16]$type   = (Get-UTF8ByteType -Byte $Byte);
   if ( ($type -ge 5) -or ($type -le 0) ) {
-    return [System.Byte[]]@();
+    return $Empty;
   }
-  [System.Int32]$vv    = (Get-UTF8ByteValue -Byte $Byte1);
-  [System.Byte[]]$bs   = @($vv); 
+  [System.Int16]$vv     = (Get-UTF8ByteValue -Byte $Byte);
+  [System.Byte[]]$bytes = @($vv); 
   if ( $type -eq 1 ) {
-    return $bs; 
+    return (,$bytes);
   }
-  if ( $type -ge 2 ) {
-    [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
-    [System.Int32]$vv     = (Get-UTF8ByteValue -Byte $Byte_p);
-    $bs += $vv;
-  } 
-  if ( $type -ge 3 ) {
-    [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
-    [System.Int32]$vv     = (Get-UTF8ByteValue -Byte $Byte_p);
-    $bs += $vv;
+  for ([System.Int16]$ii=2; $ii -le $type; $ii += 1) {
+    [System.Int16]$Byte = $Stream.ReadByte();
+    if( $Byte_p -eq -1 ) {return $Empty;}
+    [System.Int32]$vv     = (Get-UTF8ByteValue -Byte $Byte);
+    $bytes += $vv; 
   }
-  if ( $type -ge 4 ) {
-    [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
-    [System.Int32]$v      = (Get-UTF8ByteValue -Byte $Byte_p);
-    $bs += $vv;
-  }
-
-  return $bs;
+  return (,$bytes);
 }
-Export-ModuleMember -Function Get-UTF8Code;
+function Get-UTF8Code {
+  Param ( 
+    [System.Byte[]]$Bytes
+  );
+  [System.Int32]$len = $Bytes.Length; 
+  if ($len -le 0) {
+    return [System.Int32]-1; 
+  }
+  [System.Int16]$type   = (Get-UTF8ByteType -Byte $Bytes[0]);
+  if ($len -ne $type) {
+    return [System.Int32]-1; 
+  }
+  for([System.Int32]$ii=1; $ii -lt $len; $ii += 1) {
+    [System.Int16]$type = (Get-UTF8ByteType -Byte $Bytes[$ii]);
+    if($type -ne 0) {
+      return [System.Int32]-1; 
+    }
+  }
+  [System.Int32]$code = 0;
+  for([System.Int32]$ii=0; $ii -lt $len; $ii += 1) {
+    $code *= 0x40; 
+    [System.Int32]$vv = (Get-UTF8ByteValue -Byte $Bytes[$ii]);
+    $code += $vv; 
+  }
+  return $code; 
+}
+Export-ModuleMember -Function Get-FileUTF8Bytes, Get-UTF8Code;
