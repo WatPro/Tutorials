eb5b008064b1acf5773fe2002e99f538c88ded3a
WatPro <31579668+WatPro@users.noreply.github.com>
2020-07-21 00:31:31 +0800

    Update module.psm1
    * Replacing all [binary literals](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_numeric_literals?view=powershell-7 "about numeric literals") with hexadecimal literals, in order to be compatible to [version 4.0](https://docs.microsoft.com/en-gb/previous-versions/powershell/module/microsoft.powershell.core/about/about_numeric_literals?view=powershell-4.0#integer-literals "About numeric literals")

--- 202007_VersionUnicodeCharacterDatabase/05test/module.psm1
+++ 202007_VersionUnicodeCharacterDatabase/05test/module.psm1
@@ -2,15 +2,15 @@ function Get-UTF8ByteType {
   Param ( 
     [System.Byte]$Byte
   );
-  if(       ($Byte -band 0b010000000) -eq 0) {
+  if(       ($Byte -band 0x80) -eq    0) {
     [System.Int32]1;
-  } elseif (($Byte -band 0b011000000) -eq 0b010000000) { 
+  } elseif (($Byte -band 0xC0) -eq 0x80) { 
     [System.Int32]0;
-  } elseif (($Byte -band 0b011100000) -eq 0b011000000) { 
+  } elseif (($Byte -band 0xE0) -eq 0xC0) { 
     [System.Int32]2;
-  } elseif (($Byte -band 0b011110000) -eq 0b011100000) { 
+  } elseif (($Byte -band 0xF0) -eq 0xE0) { 
     [System.Int32]3;
-  } elseif (($Byte -band 0b011111000) -eq 0b011110000) { 
+  } elseif (($Byte -band 0xF8) -eq 0xF0) { 
     [System.Int32]4;
   }
 }
@@ -19,48 +19,47 @@ function Get-UTF8ByteValue {
     [System.Byte]$Byte
   );
   switch( Get-UTF8ByteType -Byte $Byte ) { 
-    0 { $Byte -band 0b000111111 }
-    1 { $Byte                   }
-    2 { $Byte -band 0b000011111 }
-    3 { $Byte -band 0b000001111 }
-    4 { $Byte -band 0b000000111 }
+    0 { $Byte -band 0x3F }
+    1 { $Byte            }
+    2 { $Byte -band 0x1F }
+    3 { $Byte -band 0x0F }
+    4 { $Byte -band 0x07 }
   }
 }
-function Get-UTF8Code {
+function Get-FileUTF8Bytes {
   Param ( 
     [System.IO.Stream]$Stream
   ); 
   [System.Int16]$Byte1 = $Stream.ReadByte(); 
-  if( $Byte1 -eq -1 ) {return -1;}
-  [System.Int32]$type  = (Get-UTF8ByteType -Byte $Byte1); 
+  if( $Byte1 -eq -1 ) {return [System.Byte[]]@();}
+  [System.Int32]$type  = (Get-UTF8ByteType -Byte $Byte1);
+  if ( ($type -ge 5) -or ($type -le 0) ) {
+    return [System.Byte[]]@();
+  }
   [System.Int32]$vv    = (Get-UTF8ByteValue -Byte $Byte1);
+  [System.Byte[]]$bs   = @($vv); 
   if ( $type -eq 1 ) {
-    return $vv; 
+    return $bs; 
   }
   if ( $type -ge 2 ) {
     [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return -1;}
-    [System.Int32]$vv_p   = (Get-UTF8ByteValue -Byte $Byte_p);
-    $vv *= 0b001000000; 
-    $vv += $vv_p;
+    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
+    [System.Int32]$vv     = (Get-UTF8ByteValue -Byte $Byte_p);
+    $bs += $vv;
   } 
   if ( $type -ge 3 ) {
     [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return -1;}
-    [System.Int32]$vv_p   = (Get-UTF8ByteValue -Byte $Byte_p);
-    $vv *= 0b001000000; 
-    $vv += $vv_p;
+    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
+    [System.Int32]$vv     = (Get-UTF8ByteValue -Byte $Byte_p);
+    $bs += $vv;
   }
   if ( $type -ge 4 ) {
     [System.Int16]$Byte_p = $Stream.ReadByte();
-    if( $Byte_p -eq -1 ) {return -1;}
-    [System.Int32]$vv_p   = (Get-UTF8ByteValue -Byte $Byte_p);
-    $vv *= 0b001000000; 
-    $vv += $vv_p;
+    if( $Byte_p -eq -1 ) {return [System.Byte[]]@();}
+    [System.Int32]$v      = (Get-UTF8ByteValue -Byte $Byte_p);
+    $bs += $vv;
   }
-  if ( ($type -ge 5) -or ($type -le 0) ) {
-    return -2;
-  }
-  return $vv;
+
+  return $bs;
 }
 Export-ModuleMember -Function Get-UTF8Code;
