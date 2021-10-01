8c76ca2dfdd18e8a0cd75d0741c9d657aceb0af4
WatPro <31579668+WatPro@users.noreply.github.com>
2021-10-01 23:33:11 +0800

    Create L596_MySQL.sql

---
+++ 202004_SQL/L596_MySQL.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# Tested on MySQL Community Server - GPL, 8.0.21
+
+SELECT 
+    `class`
+FROM 
+    `courses`
+GROUP BY 
+    `class`
+HAVING
+    COUNT(DISTINCT `student`) >= 5
+;
+
