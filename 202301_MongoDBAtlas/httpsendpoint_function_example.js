fef9c3c2ca95754660062187d1d008242ca7b17a
WatPro <31579668+WatPro@users.noreply.github.com>
2023-01-08 20:34:00 +0800

    Create httpsendpoint_function_example.js

---
+++ 202301_MongoDBAtlas/httpsendpoint_function_example.js
@@ -0,0 +1,25 @@
+exports = async function (request, response) {
+   try {
+      // 1. Parse data from the incoming request
+      if(request.body === undefined) {
+      throw new Error(`Request body was not defined.`)
+      }
+      const body = JSON.parse(request.body.text());
+      // 2. Handle the request
+      const { insertedId } = await context.services
+      .get("mongodb-atlas")
+      .db("myDb")
+      .collection("myCollection")
+      .insertOne({ date: new Date(), requestBody: body });
+      // 3. Configure the response
+      response.setStatusCode(201);
+      // tip: You can also use EJSON.stringify instead of JSON.stringify.
+      response.setBody(JSON.stringify({
+         message: "Successfully saved the request body",
+         insertedId,
+      }));
+   } catch (error) {
+      response.setStatusCode(400);
+      response.setBody(error.message);
+   }
+}
